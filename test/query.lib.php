79d2a46548dea79bc017ef788c675424813cee27
John-Mojo <johnrejimathai@gmail.com>
2013-12-05 23:39:36 +0530

    john mathai say db sucks

---
+++ test/query.lib.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+function connectDB() {
+	$dbase = mysql_connect('localhost', 'root', 'vegeta@1993') or die("Could not connect to server");
+	mysql_select_db('Timeline') or die("Could not connect to database");
+	return $dbase;
+}
+
+function mysqlQuery($query) {
+  $res = mysql_query($query) or mysql_error();
+  if(!$res) return false;
+  return $res;
+}
+
+function mysqlGetNoOfRows($resource) {
+  $rowCount = mysql_num_rows($resource);
+  if(!$rowCount) return 0;
+  return $rowCount;
+}
+
+function mysqlFetchArray($resource) {
+ $rowCount = mysql_fetch_array($resource);
+ if(!$rowCount)return 0;
+ return $rowCount;
+
+}
+
+function mysqlFetchRow($resource) {
+ $rowCount = mysql_fetch_row($resource);
+ if(!$rowCount)return 0;
+ return $rowCount;
+
+}
+
+?>
+
